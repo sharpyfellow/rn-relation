2cffc16d287b4e1539c7ce429f501bcfe8bec4c7
Trond Lind-Hansen <59420263+sharpyfellow@users.noreply.github.com>
2025-01-25 08:22:03 +0100

    Update cloudinary.js

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: 'dhb0lbi4w', // Replace with your Cloudinary cloud name
-  api_key: '977595166934669', // Replace with your Cloudinary API key
-  api_secret: 'ciHqHiRXP0PKaDJWTM-ap5QkJrw', // Replace with your Cloudinary API secret
+  cloud_name: 'xxxx', // Replace with your Cloudinary cloud name
+  api_key: 'xxxx', // Replace with your Cloudinary API key
+  api_secret: 'xxxx', // Replace with your Cloudinary API secret
 });
 
 module.exports = { cloudinary };
